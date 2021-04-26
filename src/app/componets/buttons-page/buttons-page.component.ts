75bdf9bfc7ad0b83ea0138de9c7a014e7ba2b814
thesaintzion <saintzion777@gmail.com>
2021-04-26 11:19:53 +0100

    added ag grid...

--- src/app/componets/buttons-page/buttons-page.component.ts
+++ src/app/componets/buttons-page/buttons-page.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buttons-page.component.scss']
 })
 export class ButtonsPageComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
